db5695d649a479baa87a1508923eec85033b36e3
git_demo <spranith49@gmail.com>
2024-10-18 22:08:59 +0530

    Price Prediction

--- app.py
+++ app.py
@@ -11,3 +11,8 @@ st.image("wallpaper.jpg")
 House_size = st.number_input("Please enter your House Size: ")
 
 Bedrooms = st.number_input("Please enter NO of Bedrooms: ")
+
+if st.button("Predict"):
+    pranith = np.array([[House_size, Bedrooms]])
+    haha = model.Predict(pranith)
+    st.write(f"The price of the house is {pranith[0]}")
